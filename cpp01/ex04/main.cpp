e489330dca95bb4158b5325dfe0c7c98d8d2bdfb
kali <kali@kali>
2024-05-01 10:15:29 -0800

    some work

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 01:59:14 by mflury            #+#    #+#             */
-/*   Updated: 2024/04/29 22:33:58 by mflury           ###   ########.fr       */
+/*   Updated: 2024/05/01 10:13:26 by mflury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,47 @@
 #include <fstream>
 #include <string>
 
+// just because we cant use std::replace()...
+void replaceAll(std::string &str, const std::string &from, const std::string &to) {
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+}
+
 int main(int argc, char **argv) {
-	
-	std::ifstream file(argv[1]);
-    if (!file.is_open()) {
-        std::cerr << "Failed to open the file." << std::endl;
+    if (argc != 4) {
+        std::cerr << "parameters must be \"<filename> , s1, s2\"." << std::endl;
         return 1;
     }
-
-    // Read the file line by line
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl; // Output the line
+    // Open input file
+    std::string inFileName = argv[1];
+    std::ifstream inFile(inFileName.c_str());
+    if (!inFile.is_open()) {
+        std::cerr << "Failed to open the input file." << std::endl;
+        return 1;
     }
-
-    // Close the file
-    file.close();
-
-	return 0;
+    // Open output file
+    std::string outFileName = inFileName + ".replace";
+    std::ofstream outFile(outFileName.c_str());
+    if (!outFile.is_open()) {
+        std::cerr << "Failed to create the output file." << std::endl;
+        inFile.close();
+        return 1; // Return error
+    }
+    // Strings for target and replacement
+    std::string target = argv[2];
+    std::string replacement = argv[3];
+    // Read input file line by line, replace target string, and write to output file
+    std::string line;
+    while (std::getline(inFile, line)) {
+        replaceAll(line, target, replacement);
+        outFile << line << std::endl;
+    }
+    // Close files
+    inFile.close();
+    outFile.close();
+    std::cout << "File copied with string replacement." << std::endl;
+    return 0;
 }
