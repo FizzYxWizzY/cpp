040741d01086a97ef3c99875ca88545c5643b747
kali <kali@kali>
2024-05-09 08:51:48 -0800

    some work

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -1,33 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/07 13:16:51 by mflury            #+#    #+#             */
-/*   Updated: 2024/05/09 04:11:37 by mflury           ###   ########.fr       */
+/*   Created: 2024/05/08 03:40:01 by mflury            #+#    #+#             */
+/*   Updated: 2024/05/08 14:20:09 by mflury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef Fixed_HPP
-# define Fixed_HPP
-
+#include "Fixed.hpp"
 #include <iostream>
 
-class Fixed {
-	private:
-		int _nbr;
-		static const int _precision = 8;
-	
-	public:
-		Fixed();
-		Fixed(int value);
-		~Fixed();
-		Fixed(const Fixed &src);
-		Fixed &operator=(const Fixed &rhs);
-		int getRawBits() const;
-		void setRawBits(int const raw);
-};
+int main(void) {
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
 
-#endif
+	c = b;
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
