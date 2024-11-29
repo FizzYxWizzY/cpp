d0eb7b29bca4e1da506a62b6dfbe669b1fc7e9c0
kali <kali@kali>
2024-11-29 04:02:12 -0900

    *panic*

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,23 +6,26 @@
 /*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 15:52:59 by mflury            #+#    #+#             */
-/*   Updated: 2024/11/29 02:53:19 by mflury           ###   ########.fr       */
+/*   Updated: 2024/11/29 03:50:20 by mflury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MutantStack_HPP
 # define MutantStack_HPP
 
-#include <iostream>
+#include <stack>
 
-class MutantStack {
-	private:
-		
-	public:
-		MutantStack();
-		~MutantStack();
-		MutantStack(const MutantStack &src);
-		MutantStack &operator=(const MutantStack &rhs);
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	iterator begin() { return std::stack<T>::c.begin(); }
+	iterator end() { return std::stack<T>::c.end(); }
+	const_iterator begin() const { return std::stack<T>::c.begin(); }
+	const_iterator end() const { return std::stack<T>::c.end(); }
 };
 
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,7 +6,68 @@
 /*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 02:53:26 by mflury            #+#    #+#             */
-/*   Updated: 2024/11/29 02:53:26 by mflury           ###   ########.fr       */
+/*   Updated: 2024/11/29 03:58:38 by mflury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <list>
+#include "MutantStack.hpp"
+
+int main()
+{
+	{
+		std::cout << "-- TEST MUTANTSTACK --" << std::endl;
+		MutantStack<int> mstack;
+		std::cout << "add 5 and 17" << std::endl;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << "mstack.top() = " << mstack.top() << std::endl;
+		std::cout << "mstack.pop(), stack is 5" << std::endl;
+		mstack.pop();
+		std::cout << "mstack.size() = " << mstack.size() << std::endl;
+		std::cout << "add 3, 5, 737, 0 to stack, stack is 5, 3, 5, 737, 0" << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << "- " << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "-- TEST LIST --" << std::endl;
+		std::list<int> lst;
+		std::cout << "add 5 and 17" << std::endl;
+		lst.push_back(5);
+		lst.push_back(17);
+		std::cout << "lst.back() = " << lst.back() << std::endl;
+		std::cout << "lst.pop_back(), list is 5" << std::endl;
+		lst.pop_back();
+		std::cout << "lst.size() = " << lst.size() << std::endl;
+		std::cout << "add 3, 5, 737, 0 to list, list is 5, 3, 5, 737, 0" << std::endl;
+		lst.push_back(3);
+		lst.push_back(5);
+		lst.push_back(737);
+		lst.push_back(0);
+		std::list<int>::iterator it = lst.begin();
+		std::list<int>::iterator ite = lst.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << "- " << *it << std::endl;
+			++it;
+		}
+		std::list<int> s(lst);
+	}
+	return 0;
+}
